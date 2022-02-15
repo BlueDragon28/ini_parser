41f6919b66fd10642bd7a3a7501238c4ed4e0be5
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2022-02-15 16:58:22 +0100

    add ini_parser class

---
+++ include/ini_parser.h
@@ -0,0 +1,165 @@
+#ifndef INI_PARSER_H_
+#define INI_PARSER_H_
+
+#include <string>
+#include <vector>
+
+/*
+This class is used to open a ini file
+and parsing it. It's goal is to offer
+a very simple way to open ini files.
+*/
+class ini_parser
+{
+public:
+    /*
+    Creating an instance of the ini_parser
+    class and opening a ini file passing using
+    the iniFile parameters. The isFile param is
+    indicating if the iniFile param is a file path
+    or the file data.
+    */
+    ini_parser();
+    ini_parser(const char* iniFile, bool isFile = true);
+    ini_parser(const std::string& iniFile, bool isFile = true);
+
+    /*
+    Return the ini file path or data.
+    */
+    const std::string& iniFile() const;
+
+    /*
+    Return if the data passed by the users is a
+    file path or a file data.
+    */
+    bool isFile() const;
+
+    /*
+    Return if the ini data has been parsed properly.
+    */
+    bool isParsed() const;
+
+    /*
+    Return the value of the parameters (parameters)
+    of the section (section).
+    */
+    const char* getValue(const char* section, const char* parameters, bool* isExist = nullptr) const;
+    const std::string& getValue(const std::string& section, const std::string& parameters, bool* isExist = nullptr) const;
+
+    /*
+    Set the ini file path or data and parse it.
+    */
+    void setIniFile(const char* iniFile, bool isFile);
+    void setIniFile(const std::string& iniFile, bool isFile);
+
+protected:
+    /*
+    Check whether to parse has a file
+    or has a data.
+    */
+    void parse();
+    /*
+    Parse has a file.
+    */
+    void parseFile();
+    /*
+    Parse as a data. (the data 
+    of a ini file have been passed into the parameters iniFile
+    of the constructor).
+    */
+    void parseData();
+    
+    /*
+    Parse a single line of the ini data.
+    */
+    virtual void parseLine(const std::string& line, std::string& currentSection);
+
+    /*
+    Set the value (value) of the parameters (parameters)
+    of the section (section) in the parsed list.
+    */
+    void setValue(const char* section, const char* parameters, const char* value);
+    void setValue(const std::string& section, const std::string& parameters, const std::string& value);
+
+    /*
+    Remove the parameters (parameters) of the section (section)
+    of the parsed list.
+    */
+    void removeParameters(const char* section, const char* parameters);
+    void removeParameters(const std::string& section, const std::string& parameters);
+
+    /*
+    Remove the section (section) of the parsed list.
+    */
+    void removeSection(const char* section);
+    void removeSection(const std::string& section);
+
+    /*
+    Indicate that the parsing has been successfull.
+    */
+    void parsingSuccess();
+
+    /*
+    Remove the first and last spaces of a string.
+    */
+    static void removeSpaces(std::string& value);
+
+private:
+    /*
+    A structure storing a parameters and
+    it's value. The name variable is the
+    name of the parameters and the value
+    is the value of the parameters.
+    Ex : name=value.
+    */
+    struct Parameter
+    {
+        /*
+        The name of the parameters.
+        */
+        std::string name;
+        /*
+        The value fo the parameters.
+        */
+        std::string value;
+    };
+
+    /*
+    A structure storing all the parameters
+    and the values of a section.
+    */
+    struct Section
+    {
+        /*
+        The name of the section.
+        */
+        std::string name;
+        /*
+        All the parameters of the section.
+        */
+        std::vector<Parameter> params;
+    };
+
+    /*
+    The ini file path or data.
+    */
+    std::string m_iniFile;
+    /*
+    If the m_iniFile parameters is
+    a filePath or a file data.
+    */
+    bool m_isFile;
+    /*
+    All the sections of the ini file.
+    */
+    std::vector<Section> m_sections;
+    /*
+    If the ini have been parsed properly.
+    */
+    bool m_isParsed;
+
+    // Empty string
+    static const std::string m_nullString;
+};
+
+#endif // INI_PARSER_H_
---
+++ src/ini_parser.cpp
@@ -0,0 +1,350 @@
+#include "ini_parser.h"
+#include <fstream>
+#include <sstream>
+
+const std::string ini_parser::m_nullString = std::string();
+
+ini_parser::ini_parser() :
+    m_isFile(false),
+    m_isParsed(false)
+{}
+
+ini_parser::ini_parser(const char* iniFile, bool isFile) :
+    m_iniFile(iniFile),
+    m_isFile(isFile),
+    m_isParsed(false)
+{
+    parse();
+}
+
+ini_parser::ini_parser(const std::string& iniFile, bool isFile) :
+    m_iniFile(iniFile),
+    m_isFile(isFile),
+    m_isParsed(false)
+{
+    parse();
+}
+
+void ini_parser::parse()
+{
+    // Check if the m_iniFile is a file path or a file data.
+    if (m_isFile)
+        parseFile();
+    else
+        parseData();
+}
+
+void ini_parser::parseFile()
+{
+    // Parsing the m_iniFile file.
+    if (m_iniFile.empty())
+        return;
+    
+    // Opening the ini file.
+    std::ifstream file(m_iniFile);
+    if (file.is_open())
+    {
+        std::string currentSection;
+        std::string line;
+
+        while (std::getline(file, line))
+            parseLine(line, currentSection);
+    }
+
+    parsingSuccess();
+}
+
+void ini_parser::parseData()
+{
+    // Parsing the m_iniFile as ini data.
+    if (m_iniFile.empty())
+        return;
+    
+    // Streaming the string.
+    std::istringstream data(m_iniFile);
+    std::string currentSection;
+    std::string line;
+    while (std::getline(data, line))
+        parseLine(line, currentSection);
+    
+    parsingSuccess();
+}
+
+void ini_parser::parseLine(const std::string& line, std::string& currentSection)
+{
+    // Parsing a single line of a ini file.
+    if (line.size() == 0)
+        return;
+    
+    // Retrieve the section name.
+    if (line.at(0) == '#')
+        return;
+    else if (line.at(0) == '[')
+    {
+        size_t pos = line.find_first_of(']');
+        if (pos < line.size())
+        {
+            currentSection.clear();
+            currentSection.append(
+                std::next(line.cbegin(), 1),
+                std::next(line.cbegin(), pos));
+            removeSpaces(currentSection);
+        }
+    }
+    // Retrieve the parameters and values
+    else
+    {
+        /*
+        Do not parse parameters if there are no attached
+        to any sections.
+        */
+        if (currentSection.empty())
+            return;
+
+        size_t pos = line.find_first_of('=');
+        if (pos < line.size() && pos > 0)
+        {
+            std::string paramName;
+            std::string valueName;
+
+            paramName.append(
+                line.cbegin(),
+                std::next(line.cbegin(), pos));
+            
+            if (pos+1 < line.size())
+                valueName.append(
+                    std::next(line.cbegin(), pos+1),
+                    line.cend());
+                
+            removeSpaces(paramName);
+            removeSpaces(valueName);
+            
+            setValue(currentSection, paramName, valueName);
+        }
+    }
+}
+
+void ini_parser::setValue(const char* section, const char* parameters, const char* value)
+{
+    setValue(std::string(section), std::string(parameters), std::string(value));
+}
+
+void ini_parser::setValue(const std::string& section, const std::string& parameters, const std::string& value)
+{
+    // Setting the value of the parameters of the section (section).
+    Section* pSection = nullptr;
+
+    // Find existing section.
+    for (std::vector<Section>::iterator it = m_sections.begin();
+         it != m_sections.end();
+         it++)
+    {
+        if (it->name == section)
+        {
+            pSection = &(*it);
+            break;
+        }
+    }
+
+    // If no section found, create a new one.
+    if (pSection == nullptr)
+    {
+        Section sec = {};
+        sec.name = section;
+        m_sections.push_back(sec);
+        pSection = &m_sections.at(m_sections.size()-1);
+    }
+
+    Parameter *pParam = nullptr;
+    // Find existing parameters
+    /*for (size_t i = 0; i < pSection->params.size(); i++)
+    {
+        if (pSection->params.at(i).name == parameters)
+        {
+            pParam = &pSection->params.at(i);
+            break;
+        }
+    }*/
+    for (std::vector<Parameter>::iterator it = pSection->params.begin();
+         it != pSection->params.end();
+         it++)
+    {
+        if (it->name == parameters)
+        {
+            pParam = &(*it);
+            break;
+        }
+    }
+
+    // If no parameters found, create a new one.
+    if (pParam == nullptr)
+    {
+        ini_parser::Parameter param = {};
+        param.name = parameters;
+        pSection->params.push_back(param);
+        pParam = &pSection->params.at(pSection->params.size()-1);
+    }
+
+    // Set the value of the parameters.
+    pParam->value = value;
+}
+
+void ini_parser::removeParameters(const char* section, const char* parameters)
+{
+    removeParameters(std::string(section), std::string(parameters));
+}
+
+void ini_parser::removeParameters(const std::string& section, const std::string& parameters)
+{
+    // Removing a parameters of a given section.
+    Section* pSection = nullptr;
+
+    // Find the section.
+    for (std::vector<Section>::iterator it = m_sections.begin();
+         it != m_sections.end();
+         it++)
+    {
+        if (it->name == section)
+        {
+            pSection = &(*it);
+            break;
+        }
+    }
+
+    // Remove the parameters from the section.
+    for (std::vector<Parameter>::const_iterator it = pSection->params.cbegin();
+         it != pSection->params.cend();
+         it++)
+    {
+        if (it->name == parameters)
+        {
+            pSection->params.erase(it);
+            break;
+        }
+    }
+}
+
+void ini_parser::removeSection(const char* section)
+{
+    removeSection(std::string(section));
+}
+
+void ini_parser::removeSection(const std::string& section)
+{
+    // Removing a section.
+    for (std::vector<Section>::const_iterator it = m_sections.cbegin();
+         it != m_sections.cend();
+         it++)
+    {
+        if (it->name == section)
+        {
+            m_sections.erase(it);
+            break;
+        }
+    }
+}
+
+void ini_parser::parsingSuccess()
+{
+    m_isParsed = true;
+}
+
+void ini_parser::removeSpaces(std::string& value)
+{
+    // Removing the first and the last spaces from a string.
+    // Removing the first spaces.
+    while (value.size() > 0)
+    {
+        if (value.at(0) == ' ')
+            value.erase(0, 1);
+        else
+            break;
+    }
+
+    // Removing the last spaces.
+    for (std::string::const_reverse_iterator rit = value.crbegin();
+         rit != value.crend();
+         rit++)
+    {
+        if (*rit == ' ')
+            value.erase(rit.base());
+        else
+            break;
+    }
+}
+
+const std::string& ini_parser::iniFile() const
+{
+    return m_iniFile;
+}
+
+bool ini_parser::isFile() const
+{
+    return m_isFile;
+}
+
+bool ini_parser::isParsed() const
+{
+    return m_isParsed;
+}
+
+const char* ini_parser::getValue(const char* section, const char* parameters, bool* isExist) const
+{
+    return getValue(std::string(section), std::string(parameters), isExist).c_str();
+}
+
+const std::string& ini_parser::getValue(const std::string& section, const std::string& parameters, bool* isExist) const
+{
+    // Return the value of the parameters (parameters) of the section (section).
+    // Find section.
+    const Section* pSection = nullptr;
+    for (std::vector<Section>::const_iterator it = m_sections.cbegin();
+         it != m_sections.cend();
+         it++)
+    {
+        if (it->name == section)
+        {
+            pSection = &(*it);
+            break;
+        }
+    }
+
+    if (pSection == nullptr)
+        return m_nullString;
+    
+    // Find parameters
+    const Parameter* pParameters = nullptr;
+    for (std::vector<Parameter>::const_iterator it = pSection->params.cbegin();
+         it != pSection->params.cend();
+         it++)
+    {
+        if (it->name == parameters)
+        {
+            pParameters = &(*it);
+            break;
+        }
+    }
+
+    if (pParameters == nullptr)
+        return m_nullString;
+    else
+        return pParameters->value;
+}
+
+void ini_parser::setIniFile(const char* iniFile, bool isFile)
+{
+    m_iniFile = iniFile;
+    m_isFile = isFile;
+    m_isParsed = false;
+    m_sections.clear();
+    parse();
+}
+
+void ini_parser::setIniFile(const std::string& iniFile, bool isFile)
+{
+    m_iniFile = iniFile;
+    m_isFile = isFile;
+    m_isParsed = false;
+    m_sections.clear();
+    parse();
+}
